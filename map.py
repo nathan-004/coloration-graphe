3e087105fd7493acddae5b4d19a78595db59c6f4
Nathan <n49874687@gmail.com>
2026-04-13 13:27:02 +0200

    detection regions

--- map.py
+++ map.py
@@ -1,5 +1,8 @@
 from PIL import Image
 
+from random import choice
+from collections import deque
+
 def distance(p1: tuple, p2: tuple) -> float:
     dr = p1[0] - p2[0]
     dg = p1[1] - p2[1]
@@ -38,6 +41,64 @@ def get_outlines(img_path: str, seuil_distance: float = 150, display: bool = Tru
 
     if display:
         result.show()
+    
+    return result
+
+def get_regions(img: Image, min_pixels_region: int = 150, allow_cut_regions: bool = False, display: bool = True) -> Image:
+    pixels = img.load()
+    visited = set()
+    regions = []
+    if display:
+        res = Image.new("RGB", (img.width, img.height))
+
+    n = 0
+
+    for y in range(img.height):
+        for x in range(img.width):
+            if (x, y) in visited:
+                continue
+
+            if pixels[x, y] != (255, 255, 255):
+                continue
+
+            stack = deque([(x, y)])
+            region_pixels = [] 
+
+            cut_region = False
+
+            while stack:
+                cur_x, cur_y = stack.pop()
+
+                if (cur_x, cur_y) in visited:
+                    continue
+
+                if pixels[cur_x, cur_y] != (255, 255, 255):
+                    continue
+                
+                region_pixels.append((cur_x, cur_y))
+                visited.add((cur_x, cur_y))
+
+                for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nx, ny = cur_x + dx, cur_y + dy
+
+                    if 0 <= nx < img.width and 0 <= ny < img.height:
+                        stack.append((nx, ny))
+                    else:
+                        cut_region = True
+            
+            if cut_region and not allow_cut_regions:
+                continue
+            
+            if len(region_pixels) > min_pixels_region:
+                regions.append(region_pixels)
+                if display:
+                    for x, y in region_pixels:
+                        res.putpixel((x, y), (255, 255, 255))
+
+    res.show()
+    return regions
 
 if __name__ == "__main__":
-    get_outlines("assets/imgs/regions_france.jpg")
+    img = get_outlines("assets/imgs/regions_france.jpg")
+    regions = get_regions(img)
+    print(len(regions))
