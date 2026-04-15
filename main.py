98207e355328de968b24f2bc8bf52f0cae4370af
HugoN <Hugo.naroutou86@gmail.com>
2026-04-15 14:15:01 +0200

    colorisation aléatoire

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from graph import Graphe
+from graph import Graphe, get_regions_france
 from utils import display_graph
+import random
 
-result = Graphe.from_map_image("assets/imgs/regions_france.jpg")
+#colorisation purement aléatoire
+def color_random(graph: Graphe):
+    noeuds = list(graph.keys())
+    while noeuds:
+        region = random.choice(noeuds)
+        color = random.random(),random.random(),random.random()
+        graph.colors[region] = color
+        noeuds.remove(region)
 
-print(result.graphe)
-display_graph(result.graphe)
+#result = Graphe.from_map_image("assets/imgs/regions_france.jpg")
+
+graph = get_regions_france()
+graph.colors["Hauts-de-France"]= 1,1,0.7
+
+
+color_random(graph)
+display_graph(graph)
+
