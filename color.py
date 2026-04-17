9b182038532cff0c96d1f08082ba8d12ae982b97
Nathan <n49874687@gmail.com>
2026-04-17 14:43:06 +0200

    hugo: colorisation glouton

--- color.py
+++ color.py
@@ -31,3 +31,30 @@ def color_random_rules(graph: Graphe):
             colors.append(color)
             graph.colors[region] = color
         noeuds.remove(region)
+
+def color_glouton(graph: Graphe):
+    r = 1
+    g = 0
+    b = 0
+    colors = []
+    noeuds = list(graph.keys())
+    while noeuds:
+        for i in noeuds:
+            region = i
+            for color in colors:
+                same = False
+                for voisin in graph[region]:
+                    if color == graph.colors[voisin]:
+                        same = True
+                        break
+                if same == False:
+                    graph.colors[region] = color
+                else:
+                    r = random.random()
+                    g = random.random()
+                    b = random.random()
+            if graph.colors[region] is None:
+                color = r,g,b
+                colors.append(color)
+                graph.colors[region] = color
+            noeuds.remove(region)
