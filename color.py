077b2e4485a631d2f1b2dadfccc381f618a37a75
hugonaroutou86-del <hugo.naroutou86@gmail.com>
2026-04-21 15:07:50 +0200

    color glouton

--- color.py
+++ color.py
@@ -45,36 +45,36 @@ def color_random_rules(graph: Graphe):
 
 def color_glouton(graph: Graphe):
     result = []
-    r = 1
-    g = 0
-    b = 0
-    colors = []
+    color = random.random(), random.random(), random.random()
     noeuds = list(graph.keys())
-    while noeuds:
-        for i in noeuds:
-            region = i
-            for color in colors:
-                same = False
-                for voisin in graph[region]:
-                    if color == graph.colors[voisin]:
-                        same = True
-                        break
-                if same == False:
-                    graph.colors[region] = color
-                    result.append({region : color})
-                    break
-                else:
-                    r = random.random()
-                    g = random.random()
-                    b = random.random()
-            if graph.colors[region] is None:
-                color = r,g,b
-                colors.append(color)
-                graph.colors[region] = color
-                result.append({region : color})
 
-            noeuds.remove(region)
+    while noeuds:
+        r = None
+        v_max = -float("inf")
+
+        for i in noeuds:
+            n = 0 # Nombre de voisins non coloriés
+
+            for voisin in graph[i]:
+                if graph.colors[voisin] is None:
+                    n+=1
+                elif graph.colors[voisin] == color:
+                    break
+            else:
+                if v_max <= n :
+                    v_max = n
+                    r = i
+
+        if r is None:
+            color = random.random(), random.random(), random.random()
+            continue
+
+        result.append({r: color})
+        graph.colors[r] = color
+        noeuds.remove(r)
+
     return result
+
 
 if __name__ == "__main__":
     g = Graphe.from_map_image("assets/imgs/regions_france.jpg")
