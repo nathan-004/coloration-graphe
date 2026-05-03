80c29262950053660274dc149689e64112cb6253
Nathan <n49874687@gmail.com>
2026-05-03 21:15:18 +0200

    commentaire `main.py`

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+Auteurs: Nathan
+Permet de lancer le serveur et de gérer la connection entre les programmes python et le frontend
+"""
+
 from flask import Flask, render_template, request, Response, jsonify
 from flask import Blueprint, abort
 import os
@@ -10,6 +15,7 @@ from color import *
 
 app = Flask(__name__)
 
+# Permet l'accès au dossier `imgs` depuis le site web
 imgs = Blueprint("imgs", __name__, static_url_path="/imgs", static_folder="saves/imgs")
 app.register_blueprint(imgs)
 
@@ -17,38 +23,51 @@ app.register_blueprint(imgs)
 def home():
     return render_template("index.html")
 
-@app.route("/maps")
+@app.route("/maps") # Vide
 def maps():
     return render_template("maps.html")
 
 @app.route("/maps/<id>")
 def map(id):
+    """
+    Page d'affichage de la carte
+
+    Parameters
+    ----------
+    id: str
+        résultat sha1 de l'image
+    """
     path = f"saves/{id}.json"
-    if not os.path.isfile(path):
+
+    if not os.path.isfile(path): # Aucune sauvegarde trouvée
         abort(404)
+
     with open(path) as f:
-        centers = json.load(f)["centers"]
+        centers = json.load(f)["centers"] # Récupérer centres des régions
     return render_template("map.html", id=id, centers=centers)
 
 @app.route("/upload")
 def upload():
+    """Page web d'upload d'une img"""
     return render_template("upload.html")
 
 @app.route("/upload-map", methods=["POST"])
 def upload_map():
+    """Route transmettant le fichier image donnée par l'utilisateur"""
+    # Réception du fichier
     file = request.files["file"]
     path = "assets/temp"
     i = 0
 
-    while os.path.isfile(path + str(i) + ".png"):
+    while os.path.isfile(path + str(i) + ".png"): # Choix d'un nom non utilisé
         i += 1
     path = path + str(i) + ".png"
 
-    file.save(path)
+    file.save(path) # Stockage fichier pour traitement
 
     result = Graphe.from_map_image(path)
 
-    os.remove(path)
+    os.remove(path) # Supprimer image origine après traitement
 
     return jsonify({
         "status": "success",
@@ -57,6 +76,8 @@ def upload_map():
 
 @app.route("/progress")
 def progress():
+    """Renvoie le progrès du traitement de l'image en continu"""
+
     def generate():
         last = -1
         while True:
@@ -69,6 +90,21 @@ def progress():
 
 @app.route("/color", methods=["POST"])
 def color():
+    """
+    Renvoie le résultat du coloriage du graphe
+
+    Parameters
+    ----------
+    id: str
+        sha1 de l'image à colorier
+    algo: str
+        nom de l'algo à utiliser
+
+    Returns
+    -------
+    list[dict{region_idx: color}]
+        Pour chaque étapes, la couleur attribuée
+    """
     id = request.json["id"]
     algo = request.json["algo"]
     print(id, algo)
